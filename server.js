6547f4726083ff029db2c89513cc878d5d321459
Axel Niklasson <axel.niklasson@live.com>
2016-06-04 23:48:29 +0200

    Might have added favicon. Unclear when on localhost

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var path = require('path');
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
+var favicon = require('serve-favicon');
 
 /* DB connection */
 var db = require('./config/db.js');
@@ -13,6 +14,7 @@ var app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
