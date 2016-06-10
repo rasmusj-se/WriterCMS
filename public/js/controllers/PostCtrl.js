22101a762754983e36573b776f4e4a367570f5ba
Axel Niklasson <axel.niklasson@live.com>
2016-06-10 11:18:55 +0200

    Removed compression, fixes #2

--- public/js/controllers/PostCtrl.js
+++ public/js/controllers/PostCtrl.js
@@ -107,36 +107,7 @@ module.controller('NewPostCtrl', function($scope, $stateParams, $timeout, Catego
                 var img = new Image();
 
                 reader.onload = function(e) {
-                    img.src = e.target.result;
-                    img.onload = function() {
-
-                        var canvas = document.createElement("canvas");
-                        var ctx = canvas.getContext("2d");
-                        ctx.drawImage(img, 0, 0);
-                        var MAX_WIDTH = 800;
-                        var MAX_HEIGHT = 600;
-                        var width = img.width;
-                        var height = img.height;
-
-                        if (width > height) {
-                          if (width > MAX_WIDTH) {
-                            height *= MAX_WIDTH / width;
-                            width = MAX_WIDTH;
-                          }
-                        } else {
-                          if (height > MAX_HEIGHT) {
-                            width *= MAX_HEIGHT / height;
-                            height = MAX_HEIGHT;
-                          }
-                        }
-                        canvas.width = width;
-                        canvas.height = height;
-                        var ctx = canvas.getContext("2d");
-                        ctx.drawImage(img, 0, 0, width, height);
-
-                        var base64 = canvas.toDataURL("image/png");
-                        $scope.$apply($scope.images.push(base64));
-                    }
+                    $scope.$apply($scope.images.push(this.result));
                 }
 
                 reader.readAsDataURL(file);
